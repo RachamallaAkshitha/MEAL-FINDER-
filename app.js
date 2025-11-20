21a685f43bf472191a64b282f275d69984ed652c
RachamallaAkshitha <akshitha.rachamalla@gmail.com>
2025-11-21 01:51:40 +0530

    fourth commit

--- app.js
+++ app.js
@@ -23,3 +23,43 @@ const CATEGORIES_API = "https://www.themealdb.com/api/json/v1/1/categories.php";
 const SEARCH_API = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const FILTER_API = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
 const DETAILS_API = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+// 3. LOAD CATEGORIES ON HOMEPAGE
+
+const loadCategories = async () => {
+    const box = document.getElementById("categoryList");
+    const sideList = document.getElementById("sideList");
+
+    if (!box) return;
+
+    const res = await fetch(CATEGORIES_API);
+    const data = await res.json();
+    const categories = data.categories;
+
+    categories.forEach(category => {
+        box.innerHTML += `
+            <div class="card" onclick="openCategory('${category.strCategory}')">
+                <img src="${category.strCategoryThumb}">
+                <p>${category.strCategory}</p>
+            </div>
+        `;
+
+        if (sideList) {
+            sideList.innerHTML += `
+                <li onclick="openCategory('${category.strCategory}')">
+                    ${category.strCategory}
+                </li>
+            `;
+        }
+    });
+};
+
+loadCategories();
+
+
+
+
+// 4. OPEN CATEGORY PAGE
+const openCategory = (name) => {
+    window.location.href = `category.html?c=${name}`;
+};
