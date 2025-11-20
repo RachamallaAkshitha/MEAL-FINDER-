e6ea005954801c14f8a162016d9bc822e52fd638
RachamallaAkshitha <akshitha.rachamalla@gmail.com>
2025-11-21 01:32:24 +0530

    Third commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const toggleBtn = document.getElementById("toggleBtn");
+const closeBtn = document.getElementById("closeBtn");
+const sidebar = document.getElementById("sidebar");
+
+if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+        sidebar.style.right = "0px";
+    });
+}
+
+if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+        sidebar.style.right = "-300px";
+    });
+}
+
+
+
+// --------------------------------------
+// 2. API LINKS
+// --------------------------------------
+const CATEGORIES_API = "https://www.themealdb.com/api/json/v1/1/categories.php";
+const SEARCH_API = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const FILTER_API = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+const DETAILS_API = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
